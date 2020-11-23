78adb38347a0f74c5c192614d774e12a1ccbb32f
root <root@master.example.com>
2020-11-23 19:39:47 +0000

    adding environment.conf and site.pp

---
+++ site.pp
@@ -0,0 +1,16 @@
+node 'slave.example.com' {
+  file{'info1.txt' :
+    path => '/tmp/',
+    ensure => 'present',
+    content => "new1 file to be created \n"
+    }
+  package{'httpd' :
+    ensure => 'present',
+    }
+  service{'httpd' :
+    ensure => 'running',
+    }
+  notify{
+    "This is production envirnment"
+    {
+}
